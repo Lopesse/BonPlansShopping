93caffb02012dcb3a848fdf5d96ef024026dbe6d
Djalila <djalilalomba@gmail.com>
2022-03-13 18:05:52 +0100

    Expiration

--- src/components/Annonce.jsx
+++ src/components/Annonce.jsx
@@ -2,10 +2,28 @@ import { useContext } from "react";
 import { Link } from "react-router-dom"
 import { UserContext } from "./UserContext";
 import './css/annonces.css';
+import { URLS } from "../dataBase/apiURLS";
 
 
 export default function Annonce(props) {
-    const annonce = props.annonce;    
+    const annonce = props.annonce;  
+    
+    const todayTime = Date.now();
+    const expTime = new Date(annonce.date_expiration).getTime();
+    const tempsRestant = expTime - todayTime;
+    console.log(tempsRestant);
+
+    setTimeout(() => { 
+        fetch(URLS.delete_annonce, {
+            method: "POST",
+            body: JSON.stringify({ id: annonce.id }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    }, tempsRestant);
+
+
     return (
         <>
             <div className='annonce'>
@@ -14,7 +32,7 @@ export default function Annonce(props) {
                     <h3>{annonce.titre}</h3>
                     <div>Disponible à {annonce.nom_magasin}</div>
                     <div>Publiée le : {annonce.date_creation}</div>
-                    <div>Expire le : {annonce.date_expiration}</div>
+                    <div>Expire le : {annonce.date_expiration} </div>
                     <div>Créé par : {annonce.utilisateur}</div>
                     <div>Categorie : {annonce.categorie}</div>
                     <div>Description : {annonce.description}</div>
