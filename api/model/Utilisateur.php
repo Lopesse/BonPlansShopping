f920f9946c3eaaf6610fcdd5c5246ec2f2d18016
chxsmk <91468652+chxsmk@users.noreply.github.com>
2022-03-15 17:05:51 +0100

    U

--- api/model/Utilisateur.php
+++ api/model/Utilisateur.php
@@ -141,8 +141,8 @@ class Utilisateur
 
     public function updateUser($data)
     {
-        $requete = $this->db->prepare("UPDATE utilisateur SET nom= :nom, prenom= :prenom, pseudo= :pseudo, email= :email WHERE id= :id");
-        return $requete->execute(array(":nom" => $data['nom'], ":prenom" => $data['prenom'], ":pseudo" => $data['pseudo'], ":email" => $data['email'], "id" => $data['id']));
+        $requete = $this->bd->prepare("UPDATE utilisateur SET nom= :nom, prenom= :prenom, pseudo= :pseudo, email= :email WHERE id= :id");
+        return $requete->execute(array(":nom" => $data->nom, ":prenom" => $data->prenom, ":pseudo" => $data->pseudo, ":email" => $data->email, ":id" => $data->id));
     }
 
     public function suivreCategorie($data)
